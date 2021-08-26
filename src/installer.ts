eaa819b1a3cbd1f98847f30f0d1ff6b7b45ed45e
ziyuan <hanziyuan08@outlook.com>
2021-08-26 11:33:22 +0800

    feat: complete features realization

--- src/installer.ts
+++ src/installer.ts
@@ -1,101 +1,17 @@
 import * as core from '@actions/core'
 import * as tc from '@actions/tool-cache'
-import os from 'os'
-import path from 'path'
 
-type InstallationType = 'dist' | 'manifest'
-
-export interface IGitChglogVersionInfo {
-  type: InstallationType
-  downloadUrl: string
-  resolvedVersion: string
-  fileName: string
-}
-
-export async function getGitChglog(versionSpec: string, stable: boolean, auth: string | undefined): Promise<string> {
-  // check cache
-  const toolPath = tc.find('git-chglog', versionSpec)
-  // If not found in cache, download
-  if (toolPath) {
-    core.info(`Found in cache @ ${toolPath}`)
-    return toolPath
-  }
-  core.info(`Attempting to download ${versionSpec}...`)
-  let downloadPath = ''
-  let info: IGitChglogVersionInfo | null = null
-
-  //
-  // Try download from internal distribution (popular versions only)
-  //
-  try {
-    info = await getInfoFromManifest(versionSpec, stable, auth)
-    if (info) {
-      downloadPath = await installGitChglogVersion(info, auth)
-    } else {
-      core.info('Not found in manifest.  Falling back to download directly from git-chglog')
-    }
-  } catch (err) {
-    if (err instanceof tc.HTTPError && (err.httpStatusCode === 403 || err.httpStatusCode === 429)) {
-      core.info(
-        `Received HTTP status code ${err.httpStatusCode}.  This usually indicates the rate limit has been exceeded`
-      )
-    } else {
-      core.info(err.message)
-    }
-    core.debug(err.stack)
-    core.info('Falling back to download directly from git-chglog')
-  }
-
-  return downloadPath
-}
-
-export async function getInfoFromManifest(
-  versionSpec: string,
-  stable: boolean,
-  auth: string | undefined
-): Promise<IGitChglogVersionInfo | null> {
-  let info: IGitChglogVersionInfo | null = null
-  const releases = await tc.getManifestFromRepo('git-chglog', 'git-chglog', auth, 'master')
-  core.info(`matching ${versionSpec}...`)
-  const rel = await tc.findFromManifest(versionSpec, stable, releases)
-
-  if (rel && rel.files.length > 0) {
-    info = {} as IGitChglogVersionInfo
-    info.type = 'manifest'
-    info.resolvedVersion = rel.version
-    info.downloadUrl = rel.files[0].download_url
-    info.fileName = rel.files[0].filename
-  }
-
-  return info
-}
-
-async function installGitChglogVersion(info: IGitChglogVersionInfo, auth: string | undefined): Promise<string> {
-  core.info(`Acquiring ${info.resolvedVersion} from ${info.downloadUrl}`)
-  const downloadPath = await tc.downloadTool(info.downloadUrl, undefined, auth)
+export async function installGitChglogVersion(versionSpec: string): Promise<string> {
+  const downloadUrl = `https://github.com/git-chglog/git-chglog/releases/download/v${versionSpec}/git-chglog_${versionSpec}_linux_amd64.tar.gz`
+  core.info(`Acquiring ${versionSpec} from ${downloadUrl}`)
+  const downloadPath = await tc.downloadTool(downloadUrl)
 
   core.info('Extracting git-chglog...')
-  let extPath = await extractGitChglogArchive(downloadPath)
+  const extPath = await tc.extractTar(downloadPath)
   core.info(`Successfully extracted git-chglog to ${extPath}`)
-  if (info.type === 'dist') {
-    extPath = path.join(extPath, 'git-chglog')
-  }
 
   core.info('Adding to the cache ...')
-  const cachedDir = await tc.cacheDir(extPath, 'git-chglog', info.resolvedVersion)
+  const cachedDir = await tc.cacheDir(extPath, 'git-chglog', versionSpec)
   core.info(`Successfully cached git-chglog to ${cachedDir}`)
   return cachedDir
-}
-
-export async function extractGitChglogArchive(archivePath: string): Promise<string> {
-  const platform = os.platform()
-  let extPath: string
-
-  if (platform === 'win32') {
-    extPath = await tc.extractZip(archivePath)
-  } else {
-    extPath = await tc.extractTar(archivePath)
-  }
-
-  return extPath
 }
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,6 @@
 import * as core from '@actions/core'
+import * as exec from '@actions/exec'
 import * as installer from './installer'
-
-import path from 'path'
 
 export async function run(): Promise<void> {
   try {
@@ -9,25 +8,19 @@ export async function run(): Promise<void> {
     // versionSpec is optional.  If supplied, install / use from the tool cache
     // If not supplied then problem matchers will still be setup.  Useful for self-hosted.
     //
-    const versionSpec = core.getInput('git-chglog-version')
-
-    // stable will be true unless false is the exact input
-    // since getting unstable versions should be explicit
-    const stable = (core.getInput('stable') || 'true').toUpperCase() === 'TRUE'
-
-    core.info(`Setup git-chglog ${stable ? 'stable' : ''} version spec ${versionSpec}`)
-
-    if (versionSpec) {
-      const token = core.getInput('token')
-      const auth = !token ? undefined : `token ${token}`
-
-      const installDir = await installer.getGitChglog(versionSpec, stable, auth)
-
-      core.addPath(path.join(installDir, 'bin'))
-      core.info('Added git-chglog to the path')
-
-      core.info(`Successfully setup git-chglog version ${versionSpec}`)
+    let versionSpec = core.getInput('git-chglog-version')
+    if (!versionSpec) {
+      versionSpec = '0.15.0'
     }
+
+    const installDir = await installer.installGitChglogVersion(versionSpec)
+
+    core.addPath(installDir)
+    core.info('Added git-chglog to the path')
+
+    await exec.exec('git-chglog --version')
+
+    core.info(`Successfully setup git-chglog version ${versionSpec}`)
   } catch (error) {
     core.setFailed(error.message)
   }
